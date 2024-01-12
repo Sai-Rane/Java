025bd9dc2965fa95c7cc837514ddfcb5af1688ba
Sai-Rane <sai.rane729@gmail.com>
2024-01-12 12:38:45 +0530

    bug fix

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Test {
+
     public static void main(String[] args) {
         System.out.println("Addition");
         // Taking input from User using the Scanner class
